6ccdf87fa5cd0317d97ba005fc1d8b4fd30baf83
Kishan Kumar <singhkishan9911@gmail.com>
2025-05-20 19:07:29 +0530

    add user profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Layout from './layouts/Layout';
 import React from 'react';
 import About from './pages/About';
 import ProductView from './pages/ProductView';
+import UserProfile from './pages/UserProfile';
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/products/:id" element={<ProductView />} />
+          <Route path="/profile" element={<UserProfile />} />
         </Routes>
       </Layout>
     </Router>
