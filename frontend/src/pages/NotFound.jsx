98968d069eea74c33018949e675dc15ebdd5bd54
Kishan Kumar <singhkishan9911@gmail.com>
2025-08-25 21:13:25 +0530

    updated 404 not found page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,29 +1,31 @@
-import { Link } from 'react-router-dom';
-import React from 'react';
-import notFoundImage from '../assets/photos/404.png';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center px-6 text-center bg-background text-dark">
-      <img src={notFoundImage} alt="404 illustration" className='w-80' />
+    <div className="bg-light px-6 py-28 mt-12 md:px-24">
+      <div className="max-w-3xl mx-auto text-center">
+        <div className="font-gfs-didot text-dark text-6xl md:text-8xl leading-none">
+          404
+        </div>
 
-      <h1 className="text-4xl font-bold mb-2">Whoops!</h1>
-      <p className="text-subtext mb-6 max-w-md">
-        This page is missing — but your next fire fit isn’t.
-      </p>
+        <h1 className="mt-6 text-dark text-2xl md:text-3xl">
+          page not found
+        </h1>
 
-      <Link
-        to="/"
-        className="relative inline-block mx-auto overflow-hidden py-3 px-6 text-sm font-semibold uppercase tracking-widest text-dark border border-light bg-light group enabled:cursor-pointer"
-      >
-        {/* Text */}
-        <span className="relative z-10 transition-colors duration-300 group-hover:text-light">
-          Let's take you home
-        </span>
+        <p className="mt-3 text-subtext text-sm md:text-base">
+          sorry, we can’t find the page you’re looking for.
+        </p>
 
-        {/* Sliding white panel */}
-        <span className="absolute inset-0 bg-dark transform -translate-x-full transition-transform duration-300 group-hover:translate-x-0 z-0" />
-      </Link>
+        <div className="mt-10">
+          <Link
+            to="/"
+            className="uppercase text-xs px-6 py-2 border border-dark text-dark hover:bg-dark hover:text-light"
+          >
+            back to home
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
